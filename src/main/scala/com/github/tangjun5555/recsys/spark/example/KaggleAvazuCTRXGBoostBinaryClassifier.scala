d29f1a811bcac8f490d444f032d2af27da70dd40
tangj <1844250138@qq.com>
2020-06-03 10:19:23 +0800

    update

---
+++ src/main/scala/com/github/tangjun5555/recsys/spark/example/KaggleAvazuCTRXGBoostBinaryClassifier.scala
@@ -0,0 +1,13 @@
+package com.github.tangjun5555.recsys.spark.example
+
+/**
+ * author: tangj 1844250138@qq.com
+ * time: 2020/6/3 9:59 上午
+ * description:
+ *
+ * 数据集:https://www.kaggle.com/c/avazu-ctr-prediction/data
+ *
+ */
+object KaggleAvazuCTRXGBoostBinaryClassifier {
+
+}
