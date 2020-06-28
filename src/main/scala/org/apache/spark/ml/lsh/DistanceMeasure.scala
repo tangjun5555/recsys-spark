b71bc82a46804fb76f43376e84a7a7eb11aab272
tangj <1844250138@qq.com>
2020-06-28 18:34:14 +0800

    update

--- src/main/scala/org/apache/spark/ml/lsh/DistanceMeasure.scala
+++ src/main/scala/org/apache/spark/ml/lsh/DistanceMeasure.scala
@@ -22,19 +22,31 @@ private[lsh] object InnerProductDistance extends DistanceMeasure {
 
   def compute(v1: Vector, v2: Vector): Double = {
     val dotProduct = VectorUtil.dot(v1, v2)
-
     dotProduct
   }
 
 }
 
-private[lsh] object CosineDistance extends DistanceMeasure {
+private[lsh] object CosineSimilarityDistance extends DistanceMeasure {
 
   def compute(v1: Vector, v2: Vector): Double = {
     val dotProduct = VectorUtil.dot(v1, v2)
     val norms = Vectors.norm(v1, 2) * Vectors.norm(v2, 2)
-
+    assert(norms > 0)
     dotProduct / norms
+  }
+
+}
+
+private[lsh] object JaccardSimilarityDistance extends DistanceMeasure {
+
+  def compute(v1: Vector, v2: Vector): Double = {
+    val t1 = v1.toSparse.indices.toSet
+    val t2 = v2.toSparse.indices.toSet
+    val n1 = t1.intersect(t2).size
+    val n2 = t1.union(t2).size
+    assert(n2 > 0)
+    n1.toDouble / n2
   }
 
 }
