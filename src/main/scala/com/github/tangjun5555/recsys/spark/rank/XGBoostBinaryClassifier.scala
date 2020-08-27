5eee4098401b67ef147d7b44b8667199e946f464
tangj <1844250138@qq.com>
2020-08-27 16:51:27 +0800

    update

--- src/main/scala/com/github/tangjun5555/recsys/spark/rank/XGBoostBinaryClassifier.scala
+++ src/main/scala/com/github/tangjun5555/recsys/spark/rank/XGBoostBinaryClassifier.scala
@@ -94,7 +94,7 @@ class XGBoostBinaryClassifier extends Serializable {
       .setPredictionCol(predictionColumnName)
 
       .setObjective("binary:logistic")
-      .setEvalMetric("logloss")
+      .setEvalMetric("auc")
       .setTrainTestRatio(trainTestRatio)
 
       .setNumWorkers(numWorkers)
